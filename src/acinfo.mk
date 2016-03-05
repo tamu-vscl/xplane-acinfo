255a567ce690ea1687e0e39e9a4cbc911296c10e
Joshua Harris <joshua.a.harris@tamu.edu>
2016-03-04 18:33:10 -0600

    Convert Makefile spaces to tabs.
    Fix Sublime Text stupidly converting tabs to spaces; makefiles need
    tabs.

--- src/acinfo.mk
+++ src/acinfo.mk
@@ -7,18 +7,18 @@ HEADERS := $(wildcard include/*.h)
 OBJECTS := $(SOURCES:.c=.o)
 
 src/%.o: src/%.c $(HEADERS) $(XPSDK_HEADERS)
-    echo "[acinfo] CC $<"
-    $(CC)  -Iinclude $(CPPFLAGS) $(XPSDK_CPPFLAGS) $(CFLAGS) \
+	echo "[acinfo] CC $<"
+	$(CC)  -Iinclude $(CPPFLAGS) $(XPSDK_CPPFLAGS) $(CFLAGS) \
 $(INCLUDE) $(XPSDK_INCLUDE) -o $@ -c $<
 
 ./lib/libacinfo.so: $(OBJECTS)
-    echo "[acinfo] Link acinfo"
-    $(CC) $(OBJECTS) -o $@ $(LDFLAGS) $(LIBS) $(LIBDIRS)
+	echo "[acinfo] Link acinfo"
+	$(CC) $(OBJECTS) -o $@ $(LDFLAGS) $(LIBS) $(LIBDIRS)
 
 clean-plugin:
-    echo "[acinfo] Clean"
-    rm -f $(OBJECTS)
+	echo "[acinfo] Clean"
+	rm -f $(OBJECTS)
 
 distclean-plugin: clean-plugin
-    echo "[acinfo] Distclean"
-    rm -f simlink-ng.xpl
+	echo "[acinfo] Distclean"
+	rm -f simlink-ng.xpl
